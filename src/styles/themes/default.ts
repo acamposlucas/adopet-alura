c005513dd1541b2aa36883cb2577c7b761e9d734
Lucas <lac.lucas96@gmail.com>
2022-07-14 09:18:05 -0300

    Initial commit

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,36 @@
+export const defaultTheme = {
+  colors: {
+    white: "#ffffff",
+    "blue-400": "#3772ff",
+    "green-400": "#36d6ad",
+    "red-400": "#fc7071",
+    "cream-400": "#fcf0e3",
+    "gray-500": "#737380",
+    "gray-400": "#d8d8d8",
+    "gray-300": "#ededed",
+  },
+  fontFamily: {
+    primary: "'Poppins', sans-serif",
+    secondary: "'IBM Plex Sans', sans-serif",
+  },
+  fontWeight: {
+    medium: 500,
+    regular: 400,
+  },
+  fontSize: {
+    "text-2xl": "4.25rem",
+    "text-xl": "2.625rem",
+    "text-lg": "1.625rem",
+    "text-base": "1rem",
+    "text-sm": "0.875rem",
+    "text-xs": "0.75rem",
+  },
+  lineHeight: {
+    "leading-none": 1.05,
+    "leading-tight": 1.14,
+    "leading-snug": 1.33,
+    "leading-normal": 1.5,
+    "leading-relaxed": 1.71,
+    "leading-loose": 1.84,
+  },
+};
